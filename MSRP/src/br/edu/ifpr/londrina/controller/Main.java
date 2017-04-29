caf91fec82acc005871ffe0a0cce1c9f25cc1756
Welliton Fernandes Leal <oficialsuporte@gmail.com>
2017-04-28 21:53:14 -0300

    msrp

---
+++ MSRP/src/br/edu/ifpr/londrina/controller/Main.java
@@ -0,0 +1,16 @@
+/** 
+ * Name: Welliton Fernandes Leal, 2015.
+ * On the GitHub Profile: github.com/wellfernandes
+ * Federal Institute of Education, Science and Technology
+ * Open Source Project - Modeling System Routing Protocol
+ * Londrina, Parana, Brazil
+**/
+package br.edu.ifpr.londrina.controller;
+
+//Principal Class - Just turn on the initial program
+public class Main {
+	public static void main(String[] args) {
+		Controller controller = new Controller();
+		controller.initializeView();
+	}
+}
